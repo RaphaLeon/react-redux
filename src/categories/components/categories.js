89187c8d19c9f8d5a3b791670ef2a508b0841acc
jgil <jgil@rayensalud.com>
2018-10-12 16:41:56 -0300

    adding isLoading message

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -10,6 +10,10 @@ function Categories(props) {
         <div className="Categories">
             <Search />
             {
+                props.isLoading &&
+                <p>Searching your favorite's videos</p>
+            }
+            {
                 props.search.map((item) => {
                     return <Media openModal={props.handleOpenModal} {...item.toJS()} key={item.get('id')} />
                 })
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -46,7 +46,9 @@ class Media extends PureComponent {
                     />
                     <h3 className="Media-title">{this.props.title}</h3>
                     <p className="Media-author">{this.props.author}</p>
-                </div>
+                </div>                    
+                <h3 className="Media-title">{this.props.title}</h3>
+                <p className="Media-author">{this.props.author}</p>
             </div>
         )
     }
